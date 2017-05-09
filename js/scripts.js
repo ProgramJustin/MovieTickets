e9a7488a115e231735dd80d03eea727de52e6374
Justin Gomez <programjustin@gmail.com>
2017-05-08 21:37:43 -0700

    add functionality to the time input

--- js/scripts.js
+++ js/scripts.js
@@ -4,50 +4,50 @@ function Ticket(age, time) {
   }
 
 
+
   Ticket.prototype.calCost = function(age, time) {
 
       if (age <= 17 && age >= 18 && age >= 65 && time === "11:00am") {
-
-
 
     } else if (age >= 65 || time === "11:00am") {
 
       return "7.00";
 
-      return
-    } else if (age <= 17 && "age" >= 18 &&  time === "11:00am") {
+
+    } else if (age <= 17 && age >= 18 &&  time === "11:00am") {
 
       return "7.00";
 
+    } else if (age <= 17 && age >= 18 && time === "12:00pm" || time === "5:00pm" || time === "5:30pm" || time === "6:00pm" || time === "9:00pm" || time === "9:30pm") {
+      return "11.00";
+
+
     }
-
   }
-
 
 $(document).ready(function() {
 
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var userTime = $("input:radio[name=gumpUno]:checked").val();
+    // var userTime = $("input:radio[name=gumpUno]:checked").val();
+
+    this.time = $("input:radio:checked").val();
+
 
     // var realTime = checkTime(userTime);
 
-    alert(typeof userTime);
+    alert(typeof this.time);
 
     var userAge = parseInt(prompt("Enter Age"));
 
-    var newTicket = new Ticket(userAge, userTime);
-
-    newTicket.cost = 7;
-
-    newTicket.calCost(userAge, userTime);
+    var newTicket = new Ticket(userAge, this.time);
 
 
+
+    newTicket.calCost(userAge, this.time);
 
     console.log(newTicket);
-
-
 
     // var userAge = parseInt(prompt("How old are you"));
 
